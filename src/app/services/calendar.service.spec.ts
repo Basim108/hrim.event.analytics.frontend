89c357594159cd7b52a4c6689dc2a4bd22f7779b
Basim Al-Jawahery <basimdev@gmail.com>
2022-07-27 10:43:18 +0400

    test: add tests for HrimEventService, and renamed test expectations of CalendarService

--- src/app/services/calendar.service.spec.ts
+++ src/app/services/calendar.service.spec.ts
@@ -10,7 +10,11 @@ describe('CalendarService', () => {
     service = TestBed.inject(CalendarService);
   });
 
-  it('given July 2022 should generate weeks starting from 26 June 2022', () => {
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('getWeeks should generate weeks when first day of month is not first day of week', () => {
     const month = DateTime.fromISO('2022-07-23');
     const actualWeeks = service.getWeeks(month);
     expect(actualWeeks).not.toBeNull();
@@ -36,7 +40,7 @@ describe('CalendarService', () => {
     expect(firstWeek.days[6].day).toBe(2);
   });
 
-  it('given July 2022 should generate weeks ending with 6 August 2022', () => {
+  it('getWeeks should generate weeks when last day is the last week day (sunday)', () => {
     const month = DateTime.fromISO('2022-07-23');
     const actualWeeks = service.getWeeks(month);
     expect(actualWeeks).not.toBeNull();
@@ -62,33 +66,7 @@ describe('CalendarService', () => {
     expect(lastWeek.days[6].day).toBe(6);
   });
 
-  it('given June 2022 should generate weeks starting from 29 May 2022', () => {
-    const month = DateTime.fromISO('2022-06-23');
-    const actualWeeks = service.getWeeks(month);
-    expect(actualWeeks).not.toBeNull();
-    expect(actualWeeks.length).toBeGreaterThan(0);
-    const firstWeek = actualWeeks[0];
-    expect(firstWeek).not.toBeNull();
-    expect(firstWeek.days).not.toBeNull();
-    expect(firstWeek.days.length).toBe(7);
-
-    expect(firstWeek.days[0]).not.toBeNull();
-    expect(firstWeek.days[0].day).toBe(29);
-    expect(firstWeek.days[1]).not.toBeNull();
-    expect(firstWeek.days[1].day).toBe(30);
-    expect(firstWeek.days[2]).not.toBeNull();
-    expect(firstWeek.days[2].day).toBe(31);
-    expect(firstWeek.days[3]).not.toBeNull();
-    expect(firstWeek.days[3].day).toBe(1);
-    expect(firstWeek.days[4]).not.toBeNull();
-    expect(firstWeek.days[4].day).toBe(2);
-    expect(firstWeek.days[5]).not.toBeNull();
-    expect(firstWeek.days[5].day).toBe(3);
-    expect(firstWeek.days[6]).not.toBeNull();
-    expect(firstWeek.days[6].day).toBe(4);
-  });
-
-  it('given June 2022 should generate weeks ending with 6 August 2022', () => {
+  it('getWeeks should generate weeks when last day is in the middle of a week', () => {
     const month = DateTime.fromISO('2022-06-23');
     const actualWeeks = service.getWeeks(month);
     expect(actualWeeks).not.toBeNull();
@@ -114,7 +92,7 @@ describe('CalendarService', () => {
     expect(lastWeek.days[6].day).toBe(2);
   });
 
-  it('given June 2022 should set weekOfYear of first week to 22', () => {
+  it('getWeeks should set weekOfYear', () => {
     const month = DateTime.fromISO('2022-06-23');
     const actualWeeks = service.getWeeks(month);
     expect(actualWeeks).not.toBeNull();
--- src/app/services/hrim-event.service.spec.ts
+++ src/app/services/hrim-event.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HrimEventService } from './hrim-event.service';
+import {HrimEventModel} from "../shared/hrim-event.model";
 
 describe('HrimEventService', () => {
   let service: HrimEventService;
@@ -12,5 +13,11 @@ describe('HrimEventService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('createEvent should set event id to guid', () => {
+    const event = new HrimEventModel();
+    service.createEvent(event);
+    expect(event.id).not.toBeFalsy();
   });
 });
