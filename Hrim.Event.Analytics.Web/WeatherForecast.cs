2b778d3e3af09064e2a86a44191c71b2532efcf1
Basim Al-Jawahery <basimdev@gmail.com>
2022-08-25 16:53:35 +0400

    HC-18 feat: move ng project under the Asp.Net Core web application

---
+++ Hrim.Event.Analytics.Web/WeatherForecast.cs
@@ -0,0 +1,11 @@
+namespace Hrim.Event.Analytics.Web;
+
+public class WeatherForecast {
+  public DateTime Date { get; set; }
+
+  public int TemperatureC { get; set; }
+
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+  public string? Summary { get; set; }
+}
