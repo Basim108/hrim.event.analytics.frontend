44b3ce214d715e3ff048448e375526e769647bff
Basim Al-Jawahery <basimdev@gmail.com>
2022-12-01 20:55:46 +0400

    test: add tests for AuthService

--- Hrim.Event.Analytics.Web/ClientApp/src/app/services/auth.service.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, shareReplay} from "rxjs";
 import {LogService} from "./log.service";
 import {UserProfileModel} from "../shared/user-profile.model";
 import {environment} from "../../environments/environment";
@@ -17,24 +17,26 @@ export class AuthService {
   }
 
   checkAuthentication() {
-    this.http.get<UserProfileModel>(`${environment.apiUrl}/v1/user-profile/me`, {
-      withCredentials: true
-    }).subscribe({
-      next: userProfile => {
-        this.logger.debug('User is authenticated.', userProfile)
-        this.user$.next(userProfile)
-      },
-      error: error => {
-        switch (error.status) {
-          case 0:
-          case 401:
-            this.user$.next(null);
-            break;
-          default:
-            this.logger.error(`failed get user profile request: (${error.status}) ${error.message}`, error)
-        }
-      }
-    });
+    this.http
+        .get<UserProfileModel>(`${environment.apiUrl}/v1/user-profile/me`, {
+          withCredentials: true
+        })
+        .subscribe({
+          next: userProfile => {
+            this.logger.debug('User is authenticated.', userProfile)
+            this.user$.next(userProfile)
+          },
+          error: error => {
+            switch (error.status) {
+              case 0:
+              case 401:
+                this.user$.next(null);
+                break;
+              default:
+                this.logger.error(`failed get user profile request: (${error.status}) ${error.message}`, error)
+            }
+          }
+        });
   }
 
   login(idp: ExternalIdp) {
