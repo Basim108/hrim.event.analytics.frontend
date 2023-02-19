adfdec8d4f8f44fc9db9f9237a859817753696fe
Basim Al-Jawahery <basimdev@gmail.com>
2023-02-19 20:02:28 +0400

    test: add tests for auth-menu component

--- Hrim.Event.Analytics.Web/ClientApp/src/app/auth-menu/auth-menu.component.spec.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/auth-menu/auth-menu.component.spec.ts
@@ -1,24 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { AuthMenuComponent } from './auth-menu.component';
+import {AuthMenuComponent} from './auth-menu.component'
+import {AuthService} from '../services/auth.service'
+import {LogService} from '../services/log.service'
+import {HttpClientTestingModule} from '@angular/common/http/testing'
 
 describe('AuthMenuComponent', () => {
-  let component: AuthMenuComponent;
-  let fixture: ComponentFixture<AuthMenuComponent>;
+  let component: AuthMenuComponent
+  let fixture: ComponentFixture<AuthMenuComponent>
+  let authService: AuthService
 
   beforeEach(async () => {
-    pending()
     await TestBed.configureTestingModule({
-      declarations: [ AuthMenuComponent ]
-    })
-    .compileComponents();
+                   imports: [HttpClientTestingModule],
+                   declarations: [AuthMenuComponent],
+                   providers:    [AuthService, LogService]
+                 })
+                 .compileComponents()
 
-    fixture = TestBed.createComponent(AuthMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    authService = TestBed.inject(AuthService)
+    spyOn(authService, 'login')
+    spyOn(authService, 'logout')
+    fixture   = TestBed.createComponent(AuthMenuComponent)
+    component = fixture.componentInstance
+    component.isAuthenticated = false
+    fixture.detectChanges()
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('clicked on google login should login in google', () => {
+    let button = fixture.debugElement.nativeElement.querySelector('a.login-google-btn');
+    expect(button).toBeDefined()
+    button.click();
+    expect(authService.login).toHaveBeenCalledWith('google')
+  })
+
+
+  it('clicked on facebook login should login in facebook', () => {
+    let button = fixture.debugElement.nativeElement.querySelector('a.login-facebook-btn');
+    expect(button).toBeDefined()
+    button.click();
+    expect(authService.login).toHaveBeenCalledWith('facebook')
+  })
+
+  it('clicked on logout should logout', () => {
+    component.isAuthenticated = true
+    fixture.detectChanges()
+    let button = fixture.debugElement.nativeElement.querySelector('a.logout-btn');
+    expect(button).toBeDefined()
+    button.click();
+    expect(authService.logout).toHaveBeenCalledWith()
+  })
+})
