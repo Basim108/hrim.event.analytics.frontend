aa6bffd2fc9457c54e0ddba1056a774dea52af11
Basim Al-Jawahery <basimdev@gmail.com>
2023-02-19 17:18:54 +0400

    fix: use logService instead of console and add unit tests

--- Hrim.Event.Analytics.Web/ClientApp/src/app/services/error-metadata.service.spec.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/services/error-metadata.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorMetadataService } from './error-metadata.service';
+import {LogService} from "./log.service";
 
 describe('ErrorMetadataService', () => {
   let service: ErrorMetadataService;
+  let logService: LogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LogService]
+    });
+    logService = TestBed.inject(LogService);
+    spyOn(logService, 'error')
     service = TestBed.inject(ErrorMetadataService);
   });
 
-  it('should be created', () => {
+  it('should be created/resolved', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log error message when an error was handled', () => {
+    const error = new Error('ERROR')
+    service.handleError(error)
+    expect(logService.error).toHaveBeenCalledWith('ERROR',
+      jasmine.anything());
+  });
+
+  it('should log timestamp when an error was handled', () => {
+    const error = new Error('ERROR')
+    service.handleError(error)
+    expect(logService.error).toHaveBeenCalledWith(jasmine.anything(),
+      jasmine.objectContaining({
+      timestamp: jasmine.notEmpty()
+    }));
+  });
+
+  it('should log error message in the details when an error was handled', () => {
+    const error = new Error('ERROR')
+    service.handleError(error)
+    expect(logService.error).toHaveBeenCalledWith(jasmine.anything(),
+      jasmine.objectContaining({
+        message: 'ERROR'
+      }));
   });
 });
--- Hrim.Event.Analytics.Web/ClientApp/src/app/services/error-metadata.service.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/services/error-metadata.service.ts
@@ -1,11 +1,15 @@
 import {ErrorHandler, Injectable} from '@angular/core';
+import {LogService} from "./log.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorMetadataService implements ErrorHandler {
+  constructor(private logger: LogService) {
+  }
+
   handleError(error: any): void {
-    console.error({
+    this.logger.error(error.message, {
       timestamp: new Date().toISOString(),
       message: error.message,
       zone: error.zone
