d13541d24a768ff06349bc47f994fecfcd2e2564
Basim Al-Jawahery <basimdev@gmail.com>
2023-04-19 21:30:13 +0400

    devops(cd): push to dockerhub; add health check

--- Hrim.Event.Analytics.Web/Program.cs
+++ Hrim.Event.Analytics.Web/Program.cs
@@ -1,9 +1,10 @@
 using Hrim.Event.Analytics.Web.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,6 +18,10 @@ app.UseEventAnalyticsCors(builder.Configuration);
 app.UseStaticFiles();
 app.UseRouting();
 
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    AllowCachingResponses = false
+});
 app.MapControllerRoute(name: "default",
                        pattern: "{controller}/{action=Index}/{id?}");
 
