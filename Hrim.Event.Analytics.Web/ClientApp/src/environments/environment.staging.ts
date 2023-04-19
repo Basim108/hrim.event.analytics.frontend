0463767c8010f35131d3cd3b607654bdcda7e0c1
Basim Al-Jawahery <basimdev@gmail.com>
2023-04-19 22:25:00 +0400

    devops: change api url for staging environment

--- Hrim.Event.Analytics.Web/ClientApp/src/environments/environment.staging.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   environmentName: "staging",
-  apiUrl: "https://api-stage.events.hrimsoft.com",
+  apiUrl: "https://king-prawn-app-9aq4p.ondigitalocean.app",
 };
--- Hrim.Event.Analytics.Web/Extensions/WebApplicationExtensions.cs
+++ Hrim.Event.Analytics.Web/Extensions/WebApplicationExtensions.cs
@@ -1,17 +1,20 @@
+using Hrimsoft.Core.Exceptions;
+
 #pragma warning disable CS1591
 namespace Hrim.Event.Analytics.Web.Extensions;
 
-public static class WebApplicationExtensions {
+public static class WebApplicationExtensions
+{
     /// <summary> Setups CORS </summary>
     public static void UseEventAnalyticsCors(this WebApplication app, IConfiguration appConfig) {
-        var allowedOrigins = appConfig["AllowedOrigins"];
-        if (!string.IsNullOrEmpty(allowedOrigins)) {
-            var origins = allowedOrigins.Split(";", StringSplitOptions.RemoveEmptyEntries);
-            app.UseCors(x => x.WithOrigins(origins)
-                              .WithMethods("POST", "GET")
-                               // .AllowAnyMethod()
-                              .AllowCredentials()
-                              .AllowAnyHeader());
-        }
+        var allowedOrigins = appConfig["ALLOWED_ORIGINS"];
+        if (string.IsNullOrEmpty(allowedOrigins))
+            throw new ConfigurationException(null, "ALLOWED_ORIGINS");
+        var origins = allowedOrigins.Split(";", StringSplitOptions.RemoveEmptyEntries);
+        app.UseCors(x =>
+                        x.WithOrigins(origins)
+                         .AllowAnyMethod()
+                         .AllowCredentials()
+                         .AllowAnyHeader());
     }
 }
