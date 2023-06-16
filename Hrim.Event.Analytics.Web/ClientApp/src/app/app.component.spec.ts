1e3544b0d942a85d6bf8ec2ce3c9155c66f20a05
Basim Al-Jawahery <basimdev@gmail.com>
2023-06-16 15:51:20 +0400

    HC-120 fix: add fetching crudApiUrl to app component (#12)

--- Hrim.Event.Analytics.Web/ClientApp/src/app/app.component.spec.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/app.component.spec.ts
@@ -1,18 +1,34 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {LogService} from "./services/log.service";
+import {BackendUrlService} from "./services/backend-url.service";
+import {EventTypeService} from "./services/user-event-type.service";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+    let httpTestingController: HttpTestingController
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    const crudApiUrl = 'https://crud.api'
+    
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [HttpClientTestingModule],
+            providers: [LogService, BackendUrlService]
+        })
+        httpTestingController = TestBed.inject(HttpTestingController)
+        const urlService = TestBed.inject(BackendUrlService)
+        const reqCrudApi = httpTestingController.expectOne('/backend/crud')
+        reqCrudApi.flush(crudApiUrl, {status: 200, statusText: 'Ok'})
+    });
+    
+    afterEach(() => {
+        httpTestingController.verify()
+    })
+    
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 });
