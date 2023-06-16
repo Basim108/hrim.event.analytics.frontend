a01b110ca77130b3f9145dc8e898a8ebfaacd99c
Basim Al-Jawahery <basimdev@gmail.com>
2023-06-16 15:42:22 +0400

    HC-120 fix: add fetching crudApiUrl to app component

--- Hrim.Event.Analytics.Web/ClientApp/src/app/app.component.ts
+++ Hrim.Event.Analytics.Web/ClientApp/src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {LogService} from "./services/log.service";
 import {environment} from "../environments/environment";
+import {BackendUrlService} from "./services/backend-url.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Hrim Events';
+    title = 'Hrim Events';
 
-  constructor(private logger: LogService) {
-    logger.logConstructor(this);
-    logger.debug(`current environment: ${environment.environmentName}`, environment)
-  }
+    constructor(private logger: LogService,
+                private urlService: BackendUrlService) {
+        logger.logConstructor(this);
+        logger.debug(`current environment: ${environment.environmentName}`, environment)
+        logger.debug('fetching the crudApiUrl', urlService.crudApiUrl)
+    }
 }
